acf6d22e6746e30259f630c0f3ac4f7a054d2e7e
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2021-08-03 18:35:25 -0400

    Refactor challenge 4

--- challenges/test_challenge4.py
+++ challenges/test_challenge4.py
@@ -13,16 +13,10 @@ or, for fuller output:
     pytest challenges\test_challenge4.py -rP
 """
 
-# Import Python 3.9+'s ability to type hint lists and dictionaries directly
-# into 3.7 <= Python < 3.9. Without this, you need to use
-# "from typing import List" along with "List[int]", "List[str]",
-# "from typing import Dict" along with "dict[str, int]", etc.
-from __future__ import annotations
-
 # pip installed
-import pytest_check  # Allow multiple assert failures per test
 
-# Custom imports
+import pytest
+import pytest_check  # Allow multiple assert failures per test
 
 # Custom imports
 from services.expected_fibonacci_numbers import ExpectedFibonacciNumbers
@@ -30,53 +24,37 @@ from services.expected_fibonacci_words import ExpectedFibonacciWords
 from services.fibonacci import Fibonacci
 from services.numbers_to_words import ConvertNumbertoString
 
-
-def compare_actual_vs_expected(
-        sequence_length: int, calculated_sequence_in_numbers: list[int],
-        calculated_sequence_in_words: list[str]) -> None:
-
-    expected_sequence_in_numbers = ExpectedFibonacciNumbers(
-    ).get_expected_fibonacci_sequence(sequence_length)
-
-    expected_sequence_in_words = ExpectedFibonacciWords(
-    ).get_expected_fibonacci_words(sequence_length)
-
-    for count in range(sequence_length):
-
-        actual_number = calculated_sequence_in_numbers[count]
-        expected_number = expected_sequence_in_numbers[count]
-        assert_msg1 = (f"\n\nThe Fibonncaci number for n={count} is:" +
-                       f"\n\t{actual_number}\nrather than " +
-                       f"\n\t{expected_number}\n")
-        pytest_check.equal(actual_number, expected_number, assert_msg1)
-
-        actual_words = calculated_sequence_in_words[count]
-        expected_words = expected_sequence_in_words[count]
-        assert_msg2 = (
-            f"\n\nThe Fibonncaci number for n={count} is (in words):" +
-            f"\n\t{actual_words}\nrather than " + f"\n\t{expected_words}\n")
-        pytest_check.equal(actual_words, expected_words, assert_msg2)
-
-
 # If you want less output, you can vary the DESIRED_SEQUENCE_LENGTH below
 DESIRED_SEQUENCE_LENGTH = 301  # Generates over 100,000 characters of output
 
 
-def test_fibonacci_sequence(
-        desired_sequence_length: int = DESIRED_SEQUENCE_LENGTH) -> None:
+@pytest.mark.parametrize("sequence_length", [DESIRED_SEQUENCE_LENGTH])
+def test_fibonacci_sequence(sequence_length: int) -> None:
 
-    # Calculate the Fibonacci sequence of the specified length
-    calculated_sequence_in_numbers = list(
-        Fibonacci().generate_fibonacci_sequence(desired_sequence_length))
+    actual_fibonaccis = list(
+        Fibonacci().generate_fibonacci_sequence(sequence_length))
+    expected_fibonaccis = ExpectedFibonacciNumbers(
+    ).get_expected_fibonacci_sequence(sequence_length)
+    expected_fibonaccis_as_words = ExpectedFibonacciWords(
+    ).get_expected_fibonacci_words(sequence_length)
 
-    calculated_sequence_in_words = list(
-        map(ConvertNumbertoString().number_to_words,
-            calculated_sequence_in_numbers))
-    print()
-    for entry in calculated_sequence_in_words:
-        print(entry)
-    print()
+    for num in range(sequence_length):
 
-    compare_actual_vs_expected(desired_sequence_length,
-                               calculated_sequence_in_numbers,
-                               calculated_sequence_in_words)
+        fibonacci_number = actual_fibonaccis[num]
+        fibonacci_number_words = ConvertNumbertoString().number_to_words(
+            fibonacci_number)
+        print(f"For num={num}, the Fibonacci number is {fibonacci_number} " +
+              f"- {fibonacci_number_words}")
+
+        expected_fibonacci_number = expected_fibonaccis[num]
+        msg_1 = (f"\n\nThe Fibonncaci number for n={num} is:" +
+                 f"\n\t{fibonacci_number}\nrather than:" +
+                 f"\n\t{expected_fibonacci_number}\n")
+        pytest_check.equal(fibonacci_number, expected_fibonacci_number, msg_1)
+
+        expected_fibonacci_words = expected_fibonaccis_as_words[num]
+        msg_2 = (f"\n\nThe Fibonncaci number for n={num} is (in words):" +
+                 f"\n\t{fibonacci_number_words}\nrather than:" +
+                 f"\n\t{expected_fibonacci_words}\n")
+        pytest_check.equal(fibonacci_number_words, expected_fibonacci_words,
+                           msg_2)
